42030ecbb67a2ed5a695f98aa6e1e9f1fde0157e
alilangah78 <125276533+alilangah78@users.noreply.github.com>
2024-05-08 14:38:22 +0500

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,15 @@
+//Q#4
+const qoute = "A persone  who newer made a mistake  newer tried  anything new";
+const Author = "Albert Einstein";
+console.log(`${Author} once said, ${qoute}`);
+//Q#5
+let famouse_person = "Albert Einstein";
+let qoute_2 = "A persone  who newer made a mistake  newer tried  anything new";
+let message = `${famouse_person} once said , ${qoute}`;
+console.log(message);
+//Q#6
+var person_Name = "\n\t  Ali Khan\t\n ";
+console.log(person_Name);
+var stripped = person_Name.trim();
+console.log(stripped);
+export {};
---
+++ day2.ts
@@ -0,0 +1,16 @@
+//Q#4
+ const qoute :string = "A persone  who newer made a mistake  newer tried  anything new";
+ const Author :string ="Albert Einstein";
+ console.log(`${Author} once said, ${qoute}`);
+
+//Q#5
+let famouse_person :string = "Albert Einstein";
+let qoute_2 :string ="A persone  who newer made a mistake  newer tried  anything new";
+let message:string =`${famouse_person} once said , ${qoute}`;
+console.log( message);
+//Q#6
+var person_Name ="\n\t  Ali Khan\t\n ";
+console.log(person_Name);
+var stripped = person_Name.trim();
+console.log(stripped);
+
